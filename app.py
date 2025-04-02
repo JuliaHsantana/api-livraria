6886cde4c245a29f72281dfe6dacfe02f6cc969a
Julia Helena <contato.juliahsantana@gmail.com>
2025-04-02 19:54:51 -0300

    feat: delete endponit

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 import sqlite3
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-# Criamos a aplicação Flask
-# "__name__" indica que este é o arquivo principal do nosso programa
+
 app = Flask(__name__)
-CORS(app)
 
 
-@app.route("/pagar")
+@app.route("/")
 def exibir_mensagem():
-    return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
-
-
-# @app.route("/calote")
-# def manda_o_pix():
-#     return "<h2> se a ela apagou, tá devendo!!!</h2>"
+    return "<h1>Desafio API de Livros ❤️!!!</h1>"
 
 
 def init_db():
-    # sqlite3 crie o arquivo database.db e se conecte com a variável conn (connection)
     with sqlite3.connect("database.db") as conn:
         conn.execute("""
                 CREATE TABLE IF NOT EXISTS LIVROS(
@@ -44,16 +35,32 @@ def doar():
     image_url = dados.get("image_url")
 
     if not titulo or not categoria or not autor or not image_url:
-        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
+        return jsonify({"erro": "Todos os campos são obrigatórios!"}), 400
 
     with sqlite3.connect("database.db") as conn:
         conn.execute(f"""
-        INSERT INTO LIVROS(titulo,categoria,autor,image_url)
-        VALUES("{titulo}","{categoria}","{autor}","{image_url}")
+            INSERT INTO LIVROS(titulo,categoria,autor,image_url)
+            VALUES("{titulo}","{categoria}","{autor}","{image_url}")             
 """)
-
     conn.commit()
-    return jsonify({"mensagem": "Livro cadastrado com sucesso!!"}), 201
+    return jsonify({"Mensagem": "Livro cadastrado com sucesso !!"}), 201
+
+
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM LIVROS WHERE id = ?", (id,))
+        livro = cursor.fetchone()
+
+        if not livro:
+            return jsonify({'erro': 'Livro não encontrado'}), 404
+
+        cursor.execute("DELETE FROM LIVROS WHERE id = ?", (id,))
+        conn.commit()
+
+    return jsonify({'mensagem': 'Livro excluído com sucesso'}), 200
 
 
 @app.route("/livros", methods=["GET"])
